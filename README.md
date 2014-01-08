01b27b813e508503638d8e9da22355cf8d7cf030
zhangBrook <1449282202@qq.com>
2014-01-08 20:36:06 +0800

    Using the latest version of ShareSDK
    First, the version  of ShareSDK is 2.31.
    Second, adding some english annotate.

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 ShareSDK_Example_OnekeyShare
 ============================
 
-ShareSDK最简单的集成例子，展示了如何分享，包括快捷分享和分享到指定平台；The example show how to use the ShareSDK to share.
+ShareSDK最简单的集成例子，展示了如何分享，包括快捷分享和分享到指定平台
+The example show how to use the ShareSDK to share Weibo using the library of onekeyshare ,such as SinaWeibo
+=============================
+
+2014-1-8
+1、Using the latest version of ShareSDK
+2、Adding some english annotate
--- src/cn/sharesdk/example/share/MainActivity.java
+++ src/cn/sharesdk/example/share/MainActivity.java
@@ -30,11 +30,19 @@ import cn.sharesdk.onekeyshare.OnekeyShare;
 import cn.sharesdk.sina.weibo.SinaWeibo;
 import cn.sharesdk.tencent.weibo.TencentWeibo;
 
-/**ShareSDK 官网地址 ： http://www.sharesdk.cn </br>
-/1、这是用2.11版本的sharesdk，一定注意  </br>
-/2、如果要咨询客服，请加企业QQ 4006852216 </br>
-/3、咨询客服时，请把问题描述清楚，最好附带错误信息截图 </br>
-/4、一般问题，集成文档中都有，请先看看集成文档；减少客服压力，多谢合作  ^_^
+/** 中文注释
+ * ShareSDK 官网地址 ： http://www.sharesdk.cn </br>
+ *1、这是用2.11版本的sharesdk，一定注意  </br>
+ *2、如果要咨询客服，请加企业QQ 4006852216 </br>
+ *3、咨询客服时，请把问题描述清楚，最好附带错误信息截图 </br>
+ *4、一般问题，集成文档中都有，请先看看集成文档；减少客服压力，多谢合作  ^_^</br></br></br>
+ *
+ *The password of demokey.keystore is 123456
+ **ShareSDK Official Website ： http://www.sharesdk.cn </br>
+ *1、Be carefully, this sample use the version of 2.11 sharesdk  </br>
+ *2、If you want to ask for help，please add our QQ whose number is 4006852216 </br>
+ *3、Please describe detail of the question , if you have the picture of the bugs or the bugs' log ,that is better </br>
+ *4、Usually, the answers of some normal questions is exist in our user guard pdf, please read it more carefully,thanks  ^_^
 */
 public class MainActivity extends Activity implements PlatformActionListener,Callback{
 
@@ -47,17 +55,28 @@ public class MainActivity extends Activity implements PlatformActionListener,Cal
 	public static String TEST_IMAGE;
 	
 	/**ShareSDK集成方法有两种</br>
-	 * 1、第一种是引用方式，例如引用onekeyshare项目，onekeyshare项目在引用mainlibs库</br>
+	 * 1、第一种是引用方式，例如引用OneKeyShare项目，OneKeyShare项目在引用mainlibs库</br>
 	 * 2、第二种是把onekeyshare和mainlibs集成到项目中，本例子就是用第二种方式</br>
 	 * 请看“ShareSDK 使用说明文档”，SDK下载目录中 </br>
 	 * 或者看网络集成文档 http://wiki.sharesdk.cn/Android_%E5%BF%AB%E9%80%9F%E9%9B%86%E6%88%90%E6%8C%87%E5%8D%97
 	 * 3、混淆时，把sample或者本例子的混淆代码copy过去，在proguard-project.txt文件中
 	 * 
-	 * 
 	 * 平台配置信息有三种方式：
 	 * 1、在我们后台配置各个微博平台的key
 	 * 2、在代码中配置各个微博平台的key，http://sharesdk.cn/androidDoc/cn/sharesdk/framework/ShareSDK.html
-	 * 3、在配置文件中配置，本例子里面的assets/ShareSDK.conf,	 
+	 * 3、在配置文件中配置，本例子里面的assets/ShareSDK.xml,
+	 *
+	 *
+	 * There are two function to integrate ShareSDK 
+	 * 1、First, your project add the library of OneKeyShare, and the OneKeyShare library add the library of ShareSDK
+	 * 2、Second, putting all the source of ShareSDK and Onekeyshare into your project, which don't add library
+	 * 3、If you want to obfuscate project, you can reference this sample’s proguard-project file
+	 *
+	 * There are three function to configure Weibo APP KEY
+	 * 1、 Configuring each keys of microblogging platform in our background
+	 * 2、Configuring each keys of microblogging platform in your code
+	 * 3、Configuring each keys of microblogging platform in your project of assets/ShareSDK.xml file
+	 * 
 	 */
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -69,26 +88,34 @@ public class MainActivity extends Activity implements PlatformActionListener,Cal
 		/**快捷分享</br>
 		 * 可以参考sample中的demoPage这个类</br>
 		 * 可以参考http://wiki.sharesdk.cn/Android_%E5%BF%AB%E6%8D%B7%E5%88%86%E4%BA%AB
+		 * 
+		 * 
+		 * You can reference offical sample’s demoPage class
+		 * You can also reference our wiki, http://wiki.sharesdk.cn/Android_%E5%BF%AB%E6%8D%B7%E5%88%86%E4%BA%AB
 		 */		
 		Button button1 =(Button) findViewById(R.id.button1);
-		button1.setText("新浪快捷分享，有编辑界面");
+		button1.setText("SinaWeibo share using the library of onekeyshare");
 		button1.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				//快捷分享，没有九宫格，只有编辑页
+				//Using onekeyshare to share which provide some ui
 				OnekeyShare oks = new OnekeyShare();
 				//分享时Notification的图标和文字
+				//Setting the notification of picture and content on status bar
 				oks.setNotification(R.drawable.ic_launcher, "Gtpass");
 				//设置编辑页的初始化选中平台，设置后，就没有九格宫
+				//Setting the share of weibo platform
 				oks.setPlatform(SinaWeibo.NAME);
-				//text是分享文本
-				oks.setText("ShareSDK测试能否分享");
-				//网络图片地址
+				//text是分享文本,the content to share
+				oks.setText("SinaWeibo share");
+				//网络图片地址,the picture to share
 				oks.setImageUrl("http://img.appgo.cn/imgs/sharesdk/content/2013/07/25/1374723172663.jpg");
 				//设置platform后，silent=true,没有界面，直接分享；silent=false,就有编辑界面，没有就九格宫
 				//开发者可以自己修改，玩玩
+				//If the params of platform is setted ,and siletn param is true , then it will share on background  
 				oks.setSilent(false);
-				//执行动作
+				//执行动作, Action share
 				oks.show(MainActivity.this);				
 			}
 		});
@@ -99,22 +126,23 @@ public class MainActivity extends Activity implements PlatformActionListener,Cal
 		 * http://wiki.sharesdk.cn/Android_%E5%88%86%E4%BA%AB%E5%88%B0%E6%8C%87%E5%AE%9A%E5%B9%B3%E5%8F%B0
 		 */
 		Button button2 =(Button) findViewById(R.id.button2);
-		button2.setText("腾讯直接分享，没有界面");
+		button2.setText("TecentWeibo share without ui");
 		button2.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				
-				//腾讯微博
+				//腾讯微博,Setting the params to share on TencentWeibo
 				TencentWeibo.ShareParams sp = new TencentWeibo.ShareParams();
-				//分享文本
+				//分享文本,share content
 				sp.text ="twitter test";
-				//分享网络图片
+				//分享网络图片,share picture
 				sp.imageUrl="http://img.appgo.cn/imgs/sharesdk/content/2013/07/25/1374723172663.jpg";
 				
 				Platform pf = ShareSDK.getPlatform(MainActivity.this, TencentWeibo.NAME);		
 				//设置监听，继承PlatformActionListener接口
+				//Setting listener 
 				pf.setPlatformActionListener(MainActivity.this);
-				//分享
+				//Action share
 				pf.share(sp);
 			
 			}
@@ -125,48 +153,30 @@ public class MainActivity extends Activity implements PlatformActionListener,Cal
 		 * 删除九格宫不要的平台，只要删除对应平台的jar就行
 		 */				
 		Button button3 =(Button) findViewById(R.id.button3);
-		button3.setText("快捷分享，有九格宫和编辑界面");
+		button3.setText("Using onekeyshare to share ,which has ui includes editpage and nine grid palace");
 		button3.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {				
 
 				OnekeyShare oks = new OnekeyShare();
-
-				// 分享时Notification的图标和文字
-				oks.setNotification(R.drawable.ic_launcher, "ShareSDK 集成例子");
-				// address是接收人地址，仅在信息和邮件使用
+				oks.setNotification(R.drawable.ic_launcher, "ShareSDK demo");
 				oks.setAddress("12345678901");
-				// title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
-				oks.setTitle("ShareSDK 例子标题title");
-				// titleUrl是标题的网络链接，仅在人人网和QQ空间使用
+				oks.setTitle("ShareSDK title");
 				oks.setTitleUrl("http://sharesdk.cn");
-				// text是分享文本，所有平台都需要这个字段
-				oks.setText("ShareSDK 例子文本text");
-				// imagePath是图片的本地路径，sdcard卡中图片地址
+				oks.setText("ShareSDK text");
 				oks.setImagePath(MainActivity.TEST_IMAGE);
-				// imageUrl是图片的网络路径，新浪微博、人人网、QQ空间、
-				// 微信的两个平台、Linked-In支持此字段
 				oks.setImageUrl("http://img.appgo.cn/imgs/sharesdk/content/2013/07/25/1374723172663.jpg");
-				// url仅在微信（包括好友和朋友圈）中使用
 				oks.setUrl("http://sharesdk.cn");
-				// comment是我对这条分享的评论，仅在人人网和QQ空间使用
-				oks.setComment("人人分享需要的评论");
-				// site是分享此内容的网站名称，仅在QQ空间使用
-				oks.setSite("qq空间分享的网站名称");
-				// siteUrl是分享此内容的网站地址，仅在QQ空间使用
+				oks.setComment("comment");
+				oks.setSite("site");
 				oks.setSiteUrl("http://sharesdk.cn");
-				// latitude是维度数据，仅在新浪微博、腾讯微博和Foursquare使用
 				oks.setLatitude(23.122619f);
-				// longitude是经度数据，仅在新浪微博、腾讯微博和Foursquare使用
 				oks.setLongitude(113.372338f);
-				// 是否直接分享（true则直接分享），false是有九格宫，true没有
 				oks.setSilent(false);
-
 				oks.show(MainActivity.this);
 			}
 		});
 		
-		//初始化本地图片，把图片从drawable复制到sdcard中
 		new Thread() {
 			public void run() {
 				initImagePath();
@@ -174,30 +184,30 @@ public class MainActivity extends Activity implements PlatformActionListener,Cal
 		}.start();
 	}
 
-			//把图片从drawable复制到sdcard中
-			private void initImagePath() {
-				try {
-					if (Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())
-							&& Environment.getExternalStorageDirectory().exists()) {
-						TEST_IMAGE = Environment.getExternalStorageDirectory().getAbsolutePath() + FILE_NAME;
-					}
-					else {
-						TEST_IMAGE = getApplication().getFilesDir().getAbsolutePath() + FILE_NAME;
-					}
-					File file = new File(TEST_IMAGE);
-					if (!file.exists()) {
-						file.createNewFile();
-						Bitmap pic = BitmapFactory.decodeResource(getResources(), R.drawable.pic);
-						FileOutputStream fos = new FileOutputStream(file);
-						pic.compress(CompressFormat.JPEG, 100, fos);
-						fos.flush();
-						fos.close();
-					}
-				} catch(Throwable t) {
-					t.printStackTrace();
-					TEST_IMAGE = null;
-				}
+	//把图片从drawable复制到sdcard中
+	//copy the picture from the drawable to sdcard
+	private void initImagePath() {
+		try {
+			if (Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())
+					&& Environment.getExternalStorageDirectory().exists()) {
+				TEST_IMAGE = Environment.getExternalStorageDirectory().getAbsolutePath() + FILE_NAME;
+			} else {
+				TEST_IMAGE = getApplication().getFilesDir().getAbsolutePath() + FILE_NAME;
 			}
+			File file = new File(TEST_IMAGE);
+			if (!file.exists()) {
+				file.createNewFile();
+				Bitmap pic = BitmapFactory.decodeResource(getResources(), R.drawable.pic);
+				FileOutputStream fos = new FileOutputStream(file);
+				pic.compress(CompressFormat.JPEG, 100, fos);
+				fos.flush();
+				fos.close();
+			}
+		} catch (Throwable t) {
+			t.printStackTrace();
+			TEST_IMAGE = null;
+		}
+	}
 
 	@Override
 	protected void onDestroy() {
@@ -207,9 +217,10 @@ public class MainActivity extends Activity implements PlatformActionListener,Cal
 
 	//设置监听http://sharesdk.cn/androidDoc/cn/sharesdk/framework/PlatformActionListener.html
     //监听是子线程，不能Toast，要用handler处理，不要犯这么二的错误
+	//Setting listener, http://sharesdk.cn/androidDoc/cn/sharesdk/framework/PlatformActionListener.html
+	//The listener is the child-thread that can not handle ui
 	@Override
 	public void onCancel(Platform platform, int action) {
-		//监听，去掉操作
 		Message msg = new Message();
 		msg.what = MSG_ACTION_CCALLBACK;
 		msg.arg1 = 3;
@@ -220,7 +231,6 @@ public class MainActivity extends Activity implements PlatformActionListener,Cal
 
 	@Override
 	public void onComplete(Platform platform, int action, HashMap<String, Object> arg2) {
-		//监听，完成操作
 		Message msg = new Message();
 		msg.what = MSG_ACTION_CCALLBACK;
 		msg.arg1 = 1;
@@ -232,7 +242,6 @@ public class MainActivity extends Activity implements PlatformActionListener,Cal
 	@Override
 	public void onError(Platform platform, int action, Throwable t) {
 		t.printStackTrace();
-		//监听，失败操作
 		Message msg = new Message();
 		msg.what = MSG_ACTION_CCALLBACK;
 		msg.arg1 = 2;
@@ -251,11 +260,11 @@ public class MainActivity extends Activity implements PlatformActionListener,Cal
 		break;
 		case MSG_ACTION_CCALLBACK: {
 			switch (msg.arg1) {
-				case 1: { // 成功
+				case 1: { // 成功, successful notification
 					showNotification(2000, getString(R.string.share_completed));
 				}
 				break;
-				case 2: { // 失败
+				case 2: { // 失败, fail notification
 					String expName = msg.obj.getClass().getSimpleName();
 					if ("WechatClientNotExistException".equals(expName)
 							|| "WechatTimelineNotSupportedException".equals(expName)) {
@@ -272,7 +281,7 @@ public class MainActivity extends Activity implements PlatformActionListener,Cal
 					}
 				}
 				break;
-				case 3: { // 取消
+				case 3: { // 取消, cancel notification
 					showNotification(2000, getString(R.string.share_canceled));
 				}
 				break;
@@ -290,7 +299,7 @@ public class MainActivity extends Activity implements PlatformActionListener,Cal
 	return false;
 	}
 
-	// 在状态栏提示分享操作
+	// 在状态栏提示分享操作,the notification on the status bar
 		private void showNotification(long cancelTime, String text) {
 			try {
 				Context app = getApplicationContext();
